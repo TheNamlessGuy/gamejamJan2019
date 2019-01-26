28951504482686a16c198e19271950d001ff3cfc
GoranEsmail <GoranEsmail@users.noreply.github.com>
2019-01-26 17:08:17 +0100

    added benchmark
    can benchmark time and cycles now :)

--- src/common.h
+++ src/common.h
@@ -1,9 +1,29 @@
 #pragma once
 
+#include <chrono>
+
 void EMPTY_FUNCTION(){}
 
 #define PROGRAM_NAME "aa"
 #define MAXIMUM_PERCIEVABLE_FRAMERATE (24)
+
+#define BENCHMARK BENCHMARK_TIME(1e3)
+
+#define BENCHMARK_TIME(t_mult) \
+for ( struct {\
+    int i = 1;\
+    std::chrono::steady_clock::time_point start\
+        = std::chrono::high_resolution_clock::now(); } s{};\
+        s.i--;\
+        printf( "%f\n",\
+              ( std::chrono::high_resolution_clock::now()\
+                                - s.start ).count() / (1e9*(t_mult)) ) )
+
+#define BENCHMARK_CYCLES \
+for ( struct { int i = 1; uint64_t start = __rdtsc(); } s{};\
+    s.i--;\
+    printf( "%d\n", __rdtsc() - start ) )
+
 
 template <class T>
 auto dereference_anonymous_type( T const * const x ) { return *x; }
